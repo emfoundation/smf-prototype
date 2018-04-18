e7f689ba39da9d4bad167237228d35fb1eb1862f
Alex Wijns <awijns@gmail.com>
2018-04-18 16:25:33 +0200

    Init Nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <h1>SMF Prototype</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
