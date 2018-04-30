6d80a31be7763fd01058f2782c751a9c0b828eb4
George Millard <george.millard@ellenmacarthurfoundation.org>
2018-04-30 11:26:05 +0100

    Added styling to Learner Journey Page.

--- components/EmbededContent.vue
+++ components/EmbededContent.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
-    <iframe v-if="file" v-bind:src="file" width="300" height="300" frameborder="0"></iframe>
-    <iframe v-if="link" v-bind:src="link" width="300" height="300" frameborder="0"></iframe>
-    <!-- <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQeOfMOgIupwLYwoL0oFlhzIuo5ZzSBNzXMR6WxQsfM07Z7CD_InmNhsWitbl4F5lWm1FYSBgkGR5GO/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="200" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
-    <iframe src="https://emf-assets-dev.s3.amazonaws.com/media/13/Achieving-a-circular-economy.pdf" width="80%" height="800">
-        This browser does not support PDFs. Please download the PDF to view it: <a href="https://emf-assets-dev.s3.amazonaws.com/media/13/Achieving-a-circular-economy.pdf">Download PDF</a>
-    </iframe>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/zCRKvDyyHmI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> -->
+    <iframe class="embed embed-file" v-if="file" v-bind:src="file" frameborder="0"></iframe>
+    <iframe class="embed embed-link" v-if="link" v-bind:src="link" frameborder="0"></iframe>
   </div>
 </template>
 
@@ -17,11 +12,20 @@ export default {
     file : String,
     link : String,
   }
-  
+
 }
 </script>
 
 
 <style scoped>
-
+  .embed {
+    height: 60vh;
+    width: 100%;
+    border: 1px solid black;
+    margin: 1rem 0;
+  }
+  .embed-file {
+  }
+  .embed-link {
+  }
 </style>
--- pages/learnerjourneys/_id.vue
+++ pages/learnerjourneys/_id.vue
@@ -1,41 +1,50 @@
 <template>
   <div>
+    <banner v-bind:title="static_title" v-bind:subtitle='static_subtitle'/>
+    <div class="container mb-5">
+      <back-link link="/learnerjourneys"/>
       <embeded-content v-bind:file="currentAsset.file" v-bind:link="currentAsset.link"/>
-      <button @click="previous">Previous</button>
-      <button @click="next">Next</button>
-      <br>
-
+      <div class="level">
+        <div class="level-left">
+          <button v-bind:disabled="assetIndex == 0" class="button level-item min-width is-dark" @click="previous">Previous</button>
+        </div>
+        <div class="level-right">
+          <button v-bind:disabled="assetIndex == assets.length -1" class="button level-item min-width is-dark" @click="next">Next</button>
+        </div>
+      </div>
       <section>
-        <p>{{ currentAsset.uploaded_at }}</p>
-        <h3>Part {{ assetIndex +1 }} of {{ assets.length }}</h3>
-        <h3>{{ currentAsset.name }}</h3>
-        <p>{{ currentAsset.description }}</p>
-
-        <br>
-        <p>This is about...</p>
+        <p class="has-text-weight-light mb-2">Uploaded on {{ currentAsset.uploaded_at }}</p>
+        <h3 class="title mb-0">Part {{ assetIndex +1 }} of {{ assets.length }}:</h3>
+        <h3 class="title">{{ currentAsset.name }}</h3>
+        <p v-html="currentAsset.description">{{ currentAsset.description }}</p>
+        <p class="has-text-weight-bold mt-5 mb-2">This is about...</p>
         <div class="tags">
-          <span v-for="tag in currentAsset.tags" v-bind:key="tag" class="tag is-medium">
+          <span class="tag is-medium border-1 mr-3" v-for="tag in currentAsset.tags" v-bind:key="tag">
             {{ tags[tag] }}
           </span>
         </div>
-
       </section>
+    </div>
   </div>
 </template>
 
 <script>
+import BackLink from '~/components/UI/BackLink'
+import Banner from '~/components/Banner'
 import EmbededContent from '~/components/EmbededContent'
 
 export default {
   data () {
     return {
+      static_title : 'Learner Journey Name here!',
+      static_subtitle: 'Description of what a learner journey is all about.',
       assets : [
         {
           "id": 170,
           "name": "bar",
           "file": "",
           "link": "https://www.youtube.com/embed/zCRKvDyyHmI",
-          "description": "hkjh skjdfh skjdfh skdjfhs kdjfhs kjdhf ",
+          "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique ante vitae ante porttitor, vel cursus elit tempus. Donec vel venenatis nunc. Morbi condimentum ante sit amet erat vestibulum vulputate. In felis justo, luctus ac ultrices at, cursus quis turpis. Proin rhoncus convallis enim eu posuere. Sed eget ligula venenatis, suscipit velit quis, luctus justo. Nulla non lacinia neque. Praesent eget dui dui. Integer eu sapien sit amet tortor pharetra posuere. In eu bibendum nisl. Mauris bibendum molestie erat, id condimentum justo hendrerit quis. Ut eget velit eros. <br><br>Nulla iaculis diam volutpat orci viverra porttitor. Vivamus accumsan tristique enim semper pulvinar. Etiam in egestas arcu. Nam in auctor nisi. Aenean eu suscipit massa, quis facilisis quam. Nunc nec pretium ex. Fusce ipsum est, molestie eget nulla quis, sollicitudin molestie lorem.",
           "duration": null,
           "creation_date": null,
           "copyright_info": "",
@@ -55,7 +64,7 @@ export default {
           "name": "foo",
           "file": "https://emf-assets-dev.s3.amazonaws.com/media/13/Achieving-a-circular-economy.pdf",
           "link": "",
-          "description": "kjh ksjdhf skjdfh skjfhs kdjfhds kfjh",
+          "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique ante vitae ante porttitor, vel cursus elit tempus. Donec vel venenatis nunc. Morbi condimentum ante sit amet erat vestibulum vulputate. In felis justo, luctus ac ultrices at, cursus quis turpis. Proin rhoncus convallis enim eu posuere. Sed eget ligula venenatis, suscipit velit quis, luctus justo. Nulla non lacinia neque. Praesent eget dui dui. Integer eu sapien sit amet tortor pharetra posuere. In eu bibendum nisl. Mauris bibendum molestie erat, id condimentum justo hendrerit quis. Ut eget velit eros. <br><br>Nulla iaculis diam volutpat orci viverra porttitor. Vivamus accumsan tristique enim semper pulvinar. Etiam in egestas arcu. Nam in auctor nisi. Aenean eu suscipit massa, quis facilisis quam. Nunc nec pretium ex. Fusce ipsum est, molestie eget nulla quis, sollicitudin molestie lorem.",
           "duration": null,
           "creation_date": null,
           "copyright_info": "",
@@ -75,7 +84,7 @@ export default {
           "name": "rum",
           "file": "",
           "link": "https://docs.google.com/presentation/d/e/2PACX-1vQeOfMOgIupwLYwoL0oFlhzIuo5ZzSBNzXMR6WxQsfM07Z7CD_InmNhsWitbl4F5lWm1FYSBgkGR5GO/embed?start=false&loop=false&delayms=60000",
-          "description": "jh kjhsdf kjshdf kjshdf skjdfh k",
+          "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique ante vitae ante porttitor, vel cursus elit tempus. Donec vel venenatis nunc. Morbi condimentum ante sit amet erat vestibulum vulputate. In felis justo, luctus ac ultrices at, cursus quis turpis. Proin rhoncus convallis enim eu posuere. Sed eget ligula venenatis, suscipit velit quis, luctus justo. Nulla non lacinia neque. Praesent eget dui dui. Integer eu sapien sit amet tortor pharetra posuere. In eu bibendum nisl. Mauris bibendum molestie erat, id condimentum justo hendrerit quis. Ut eget velit eros. <br><br>Nulla iaculis diam volutpat orci viverra porttitor. Vivamus accumsan tristique enim semper pulvinar. Etiam in egestas arcu. Nam in auctor nisi. Aenean eu suscipit massa, quis facilisis quam. Nunc nec pretium ex. Fusce ipsum est, molestie eget nulla quis, sollicitudin molestie lorem.",
           "duration": null,
           "creation_date": null,
           "copyright_info": "",
@@ -98,7 +107,7 @@ export default {
         3 : 'Biomimicry',
         4 : 'Star Wars',
         5 : 'LoTR',
-      } 
+      }
     }
   },
   computed : {
@@ -107,6 +116,8 @@ export default {
     }
   },
   components : {
+    BackLink,
+    Banner,
     EmbededContent,
   },
   methods : {
@@ -128,7 +139,38 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .border-1 {
+    border: 1px solid grey;
+  }
+  .mb-0 {
+    margin-bottom: 0;
+  }
+  .mb-1 {
+    margin-bottom: 0.25rem;
+  }
+  .mb-2 {
+    margin-bottom: 0.5rem;
+  }
+  .mb-5 {
+    margin-bottom: 2rem;
+  }
+  .mr-1 {
+    margin-right: 0.25rem !important;
+  }
+  .mr-2 {
+    margin-right: 0.5rem !important;
+  }
+  .mr-3 {
+    margin-right: 0.75rem !important;
+  }
+  .mr-4 {
+    margin-right: 1rem !important;
+  }
+  .mt-5 {
+    margin-top: 2rem;
+  }
+  .min-width {
+    min-width: 100px;
+  }
 </style>
-
