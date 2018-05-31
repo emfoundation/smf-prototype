333fbf39d7a910341ab7bdc0255515b2c70d4a78
George Millard <george.millard@ellenmacarthurfoundation.org>
2018-05-31 11:19:01 +0100

    API Questions WIP.

--- pages/answers/_id.vue
+++ pages/answers/_id.vue
@@ -1,17 +1,17 @@
 <template>
   <section>
     <banner
-      :title="asset.name"
-      :subtitle="subtitle"
+      :title="question.name"
+      :subtitle="answer.question_context"
       :back-to="'/questions/' + $route.query.q" />
     <div class="wrap">
-      <h3 class="title">{{ asset.name }}</h3>
-      <p class="mb-5 main-text">{{ asset.description }}</p>
+      <h3 class="title">{{ answer.title }}</h3>
       <embedded-content
         :file="
         asset.file"
         :link="asset.link"
       />
+      <p class="mb-5 main-text">{{ answer.description }}</p>
       <section v-if="asset.tags.length > 0">
         <h4 class="mb-2">This is about...</h4>
         <AssetTags :tags="asset.tags"/>
@@ -26,51 +26,79 @@ import EmbeddedContent from "~/components/assets/EmbeddedContent";
 import FullscreenButton from "~/components/UI/buttons/FullscreenButton";
 
 export default {
-  asyncData(context, callback) {
-    callback(null, {
-      // Asset name would need to be retrieved from the API, for now it is taken from the route
-      subtitle:
-        "How can we change our linear ways and kickstart the Circular Economy from the very beginning, by leveraging Design thinking.",
-      asset: {
-        id: 170,
-        name: "Linear Economy - Simplified",
-        file: "https://placeimg.com/800/400/arch.png",
-        link: "",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique ante vitae ante porttitor, vel cursus elit tempus. Donec vel venenatis nunc. Morbi condimentum ante sit amet erat vestibulum vulputate. In felis justo, luctus ac ultrices at, cursus quis turpis. Proin rhoncus convallis enim eu posuere. Sed eget ligula venenatis, suscipit velit quis, luctus justo. Nulla non lacinia neque. Praesent eget dui dui. Integer eu sapien sit amet tortor pharetra posuere. In eu bibendum nisl. Mauris bibendum molestie erat, id condimentum justo hendrerit quis. Ut eget velit eros. <br><br>Nulla iaculis diam volutpat orci viverra porttitor. Vivamus accumsan tristique enim semper pulvinar. Etiam in egestas arcu. Nam in auctor nisi. Aenean eu suscipit massa, quis facilisis quam. Nunc nec pretium ex. Fusce ipsum est, molestie eget nulla quis, sollicitudin molestie lorem.",
-        duration: null,
-        creation_date: null,
-        copyright_info: "",
-        enabled: true,
-        status: "",
-        type_field: "OT",
-        filetype: "",
-        uploaded_at: "2017-04-14T14:20:23.598593Z",
-        last_edit_at: null,
-        tags: [2, 5],
-        locations: [],
-        contributors: [],
-        collections: []
-      },
-      tags: {
-        1: "Fashion",
-        2: "Circular Fibres",
-        3: "Design Thinking",
-        4: "Biocycle",
-        5: "IoT"
-      }
-    });
-  },
-  components: {
-    AssetTags,
-    Banner,
-    EmbeddedContent,
-    FullscreenButton
-  },
+  // asyncData(context, callback) {
+  //   callback(null, {
+  //     // Asset name would need to be retrieved from the API, for now it is taken from the route
+  //     subtitle:
+  //       "How can we change our linear ways and kickstart the Circular Economy from the very beginning, by leveraging Design thinking.",
+  //     asset: {
+  //       id: 170,
+  //       name: "Linear Economy - Simplified",
+  //       file: "https://placeimg.com/800/400/arch.png",
+  //       link: "",
+  //       description:
+  //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique ante vitae ante porttitor, vel cursus elit tempus. Donec vel venenatis nunc. Morbi condimentum ante sit amet erat vestibulum vulputate. In felis justo, luctus ac ultrices at, cursus quis turpis. Proin rhoncus convallis enim eu posuere. Sed eget ligula venenatis, suscipit velit quis, luctus justo. Nulla non lacinia neque. Praesent eget dui dui. Integer eu sapien sit amet tortor pharetra posuere. In eu bibendum nisl. Mauris bibendum molestie erat, id condimentum justo hendrerit quis. Ut eget velit eros. <br><br>Nulla iaculis diam volutpat orci viverra porttitor. Vivamus accumsan tristique enim semper pulvinar. Etiam in egestas arcu. Nam in auctor nisi. Aenean eu suscipit massa, quis facilisis quam. Nunc nec pretium ex. Fusce ipsum est, molestie eget nulla quis, sollicitudin molestie lorem.",
+  //       duration: null,
+  //       creation_date: null,
+  //       copyright_info: "",
+  //       enabled: true,
+  //       status: "",
+  //       type_field: "OT",
+  //       filetype: "",
+  //       uploaded_at: "2017-04-14T14:20:23.598593Z",
+  //       last_edit_at: null,
+  //       tags: [2, 5],
+  //       locations: [],
+  //       contributors: [],
+  //       collections: []
+  //     },
+  //     tags: {
+  //       1: "Fashion",
+  //       2: "Circular Fibres",
+  //       3: "Design Thinking",
+  //       4: "Biocycle",
+  //       5: "IoT"
+  //     }
+  //   });
+  // },
+  // components: {
+  //   AssetTags,
+  //   Banner,
+  //   EmbeddedContent,
+  //   FullscreenButton
+  // },
   data() {
     return {
       isFullscreen: false
     };
+  },
+  asyncData(context) {
+    let getRequests = [
+      context.$axios.$get(
+        process.env.API_BASE_URL + "questions/" + context.query.q
+      ),
+      context.$axios.$get(
+        process.env.API_BASE_URL + "answers/" + context.params.id
+      ),
+      context.$axios.$get(
+        process.env.API_BASE_URL + "asset/answer/" + context.params.id
+      )
+    ];
+
+    let returnedData = Promise.all(getRequests);
+    return returnedData
+      .then(res => {
+        let question = res[0];
+        let answer = res[1];
+        let asset = res[2];
+
+        return {
+          question,
+          answer,
+          asset
+        };
+      })
+      .catch(console.error);
   }
 };
 </script>
