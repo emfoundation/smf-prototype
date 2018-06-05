9eaf29b5ed70be55d44c5e7e10d9b91c8160713e
George Millard <george.millard@ellenmacarthurfoundation.org>
2018-06-05 13:37:46 +0100

    About Page WIP.

---
+++ pages/about/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <banner
+      :title="title"
+      :image="bannerImage"
+    />
+  </div>
+</template>
+
+<script>
+import Banner from "~/components/Banner";
+
+export default {
+  components: {
+    Banner
+  },
+  data: function() {
+    return {
+      title: "About Us",
+      bannerImage: "https://placeimg.com/1000/300/arch"
+    };
+  }
+};
+</script>
+
+<style lang="css">
+</style>
