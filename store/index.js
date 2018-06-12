f823b899479cae6783d172e71c735b7c8e1848b5
George Millard <george.millard@ellenmacarthurfoundation.org>
2018-06-12 10:10:46 +0100

    WIP Random list order.

--- store/index.js
+++ store/index.js
@@ -3,15 +3,33 @@ import Vuex from "vuex";
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      tags: []
+      tags: [],
+      learnerJourneys: []
     },
     mutations: {
       setTags(state, tags) {
         state.tags = tags;
+      },
+      setLearnerJourneys(state, learnerJourneys) {
+        state.learnerJourneys = learnerJourneys;
       }
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
+        let getRequests = [
+          this.$axios.get("http://staging.circulareconomy.space/api/tags/"),
+          this.$axios.get(
+            "http://staging.circulareconomy.space/api/learner-journeys/"
+          )
+        ];
+
+        let returnedData = Promise.all(getRequests)
+
+        return returnedData
+          .then( res => {
+            let 
+          })
+
         return this.$axios
           .get("http://staging.circulareconomy.space/api/tags/")
           .then(res => {
