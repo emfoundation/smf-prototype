a7310fbc4e07c43609acf208395785b3b78dbfa4
Merton Lansley <merty.lansley@ellenmacarthurfoundation.org>
2018-08-03 18:12:39 +0100

    Revert "Feature/138 terms and conditions"

--- pages/search.vue
+++ pages/search.vue
@@ -5,23 +5,36 @@
         <h2 class="title is-1 has-text-white"><span class="has-text-primary has-text-weight-normal">Thanks</span> for your interest</h2>
       </div>
       <h3 class="title is-3 has-text-weight-normal has-text-white">Search coming soon</h3>
-      <back-button label="Take me back!"/>
+      <div
+        class="title button is-rounded is-outlined is-4 has-text-weight-normal"
+        @click="goBack">Take me back!
+      </div>
 
     </section>
   </div>
 </template>
 
 <script>
-import BackButton from "~/components/UI/buttons/BackButton";
-
 export default {
-  components: {
-    BackButton
+  methods: {
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.go("/");
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.button {
+  color: white;
+  background-color: #495062;
+  border-width: 2px;
+  &:hover {
+    color: black !important;
+    background-color: white;
+    border-color: white;
+  }
+}
 .search {
   min-height: 100vh;
 }
@@ -37,6 +50,7 @@ export default {
     margin-left: 15%;
     width: 70%;
   }
+
   .search-heading {
     border-bottom: 1px white solid;
     padding-bottom: 1.5rem;
